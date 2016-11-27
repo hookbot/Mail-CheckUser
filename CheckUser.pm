530b2d6b3fa312b97184f83ca69d8e67dad086dd
Rob Brown <rob@betterservers.com>
2016-11-26 21:23:12 -0700

    Version 1.23

--- CheckUser.pm
+++ CheckUser.pm
@@ -28,7 +28,7 @@ $EXPORT_TAGS{constants} = [qw(CU_OK
                               CU_TRY_AGAIN)];
 push @EXPORT_OK, @{$EXPORT_TAGS{constants}};
 
-$VERSION = '1.22';
+$VERSION = '1.23';
 
 use Carp;
 use Net::DNS;
